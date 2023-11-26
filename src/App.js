7754e3eb1e990dc1dbd4bbc9389cacd6227d691f
121066 <1318720440@qq.com>
2023-11-26 17:45:50 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import { useState } from "react";
-import { Button } from "antd";
+import { useState } from 'react'
+import { Button } from 'antd'
 const InitCretet = () => {
-  const [count, setCount] = useState(0)
-  let i = 0
-  let i1 = 0
-  if (i == i1) {
-       console.log( '你话')
-  }
-  return (
-    <>
-      <div>测试{count}</div>
-      <Button>增加</Button>
-    </>
-  )
+    const [count, setCount] = useState(0)
+    let i = 0
+    let i1 = 0
+    if (i == i1) {
+        console.log('你话')
+    }
+    return (
+        <>
+            <div>测试{count}</div>
+            <Button>增加</Button>
+        </>
+    )
 }
 function App() {
-  return (
-    <div className="App">
-      <InitCretet></InitCretet>
-    </div>
-  );
+    return (
+        <div className="App">
+            <InitCretet></InitCretet>
+        </div>
+    )
 }
 
-export default App;
+export default App
