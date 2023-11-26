6ea6a325b432e9568cfe70d2acdcc2045736b6ef
121066 <1318720440@qq.com>
2023-11-26 17:22:18 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import { Button } from "antd";
+const InitCretet = () => {
+  const [count, setCount] = useState(0)
+  let i = 0
+  let i1 = 0
+  let i2 = i1 == i
+  console.log(i2)
+  return (
+    <>
+      <div>测试{count}</div>
+      <Button>增加</Button>
+    </>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InitCretet></InitCretet>
     </div>
   );
 }
