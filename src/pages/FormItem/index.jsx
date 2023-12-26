17987717a318df42954927311d53d3cf53992457
121066 <1318720440@qq.com>
2023-12-26 08:58:17 +0800

    fix:表单修改

--- src/pages/FormItem/index.jsx
+++ src/pages/FormItem/index.jsx
@@ -7,10 +7,14 @@ function FormItem(){
     const addBtn=()=>{
         form.submit()
         console.log(form.getFieldValue())
+        // form.isFieldValidating()
+    }
+    const initClick=(e,value)=>{
+        console.log(e,value)
     }
     return <>
     <div>
-        <Form onFinish={onFinish} form={form}>
+        <Form onFinish={onFinish} form={form} validateTrigger="onBlur">
            <Row gutter={24}>
             <Col span={8}>
             <Form.Item name="userName" >
@@ -18,13 +22,19 @@ function FormItem(){
             </Form.Item>
             </Col>
             <Col span={8}>
-            <Form.Item name="passWord" >
-                <Input size="large" placeholder="请输入密码"></Input>
+            <Form.Item name="passWord"  rules={[{ required: true, message: "请输入验证码" }]}>
+                <Input size="large"  placeholder="请输入密码"></Input>
+            </Form.Item>
+            </Col>
+            <Col>
+            <Form.Item>
+            <Button type="primary" onClick={addBtn}  >点击</Button>
             </Form.Item>
             </Col>
            </Row>
         </Form>
-        <Button type="primary" onClick={addBtn}>点击</Button>
+      <Button type="primary" onClick={(e)=>initClick('1',e)}>点击bind</Button>
+      <Button type="primary" onClick={initClick.bind(this,1)}>点击</Button>
     </div>
     </>
 }
