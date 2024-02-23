eab182d3923a1f030c2a958f9d6df76b3b9a6880
121066 <1318720440@qq.com>
2024-02-23 19:38:42 +0800

    fix: 添加字符转义

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,7 @@
 console.log('执行2')
 export const init = [12]
+export const element = document.createElement('span')
+export const extractNumbersAndChars = (str) => {
+    const matches = str.match(/(\d+)|(\D+)/g);
+    return matches || []; // 如果没有匹配到任何内容，返回空数组
+}
--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,8 +1,30 @@
+import { Button } from "antd"
+import { useState } from "react"
+import { element, extractNumbersAndChars } from "constants"
+const initHTML = (html, value) => {
+    console.log(html, value)
+    let arr = extractNumbersAndChars('总金额100，共2件')
+    return arr.map((item, index) => {
+        return <span className={(!isNaN(item)) ? 'bg-red-700' : ''} key={index}>{item}</span>
+    })
+
+
+}
 function HeaderTop() {
+    const i = 0
+    const initObj = {
+        0: `<span className=" bg-red-700">${i}</span>`
+    }
+    const [count, setCount] = useState(0)
+    const getInit = () => {
+        setCount(count + 1)
+    }
     return (
         <>
-            <div className="  m-2  font-bold text-3xl">
-                头部
+            <div className="  m-2  font-bold text-3xl" id="html">
+                {initHTML(element, '我已经<div className=" bg-red-700">添加了</div>')}
+                {/* {initObj[0]} */}
+                <Button onClick={getInit}>点击{count}</Button>
             </div>
         </>
     )
