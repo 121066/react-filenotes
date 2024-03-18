4a2d517b2363e005a1f3a495a9418fbe642a3ea5
121066 <1318720440@qq.com>
2024-03-18 17:16:02 +0800

    fix: 添加sass循环写法示例

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './module/user'
+import goodslist from './module/goodslist'
 export default configureStore({
     reducer: {
         user: userReducer,
+        goodslist: goodslist
     },
 })
