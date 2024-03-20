390d230f49d86ee16c4f9f25354e8573bedc3c1a
121066 <1318720440@qq.com>
2024-03-20 09:09:52 +0800

    fix: 添加tag脚本

--- tag.sh
+++ tag.sh
@@ -1,16 +1,17 @@
 prefix="text-"
 if `git status | grep "master"&/dev/null`; then
-prefix="pro-master"
+ prefix="pro-master"
 fi
 if [$1] && [$1='pre-'];then
-prefix="pre-"
+  prefix="pre-"
 fi
-function mi-tag() {
+function db-tag() {
     git push 
     git pull --tags 
-    local new_tag=$(echo ${prefix}db-$(date +'%Y-%m-%d')-$(git tag -l "${prefix}db-$(date +'%Y-%m-%d)-*" | wc -l| xargs printf "%s))
+    local new_tag=$(echo ${prefix}db-$(date +'%Y-%m-%d'))
     echo ${new_tag}
     git tag ${new_tag}
     git push origin $new_tag
 }    
 db-tag;
+# -$(git tag -l "${prefix}db-$(date +'%Y-%m-%d)-*" | wc -l| xargs printf "%s)
