0cbfce5005ce8d718734b75c4d4ffb5480a2f214
121066 <1318720440@qq.com>
2024-04-02 09:31:42 +0800

    feat: 添加表单实现

--- src/pages/EditorText/index.js
+++ src/pages/EditorText/index.js
@@ -10,6 +10,32 @@ const InitText = () => {
         </div>
     )
 }
+function UncontrolledForm() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const { firstName, email, address } = Object.fromEntries(formData);
+        console.log(Object.fromEntries(formData), firstName, email, address)
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>First Name:</label>
+            <input type="text" name="firstName" />
+
+            <label>Last Name:</label>
+            <input type="text" name="lastName" />
+
+            <label>Email:</label>
+            <input type="email" name="email" />
+
+            <label>Address:</label>
+            <input type="text" name="address" />
+            {/* ... 可能会有更多的字段 */}
+            <button type="submit">Submit</button>
+        </form>
+    );
+}
 function EditorText() {
     const { sendMessage } = useMessage()
     const sendMsg = () => {
@@ -20,6 +46,7 @@ function EditorText() {
             <div>文本编辑</div>
             <Button type="primary" onClick={sendMsg}>点击发送消息</Button>
             <InitText></InitText>
+            <UncontrolledForm></UncontrolledForm>
         </>
     )
 }
